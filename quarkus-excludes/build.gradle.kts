a87ae629979388d0212f578040c0e9e416863219
Benedikt Ritter <benedikt@gradle.com>
2022-10-28 09:54:58 +0200

    Fix sample by configuring excludes on all configurations

--- quarkus-excludes/build.gradle.kts
+++ quarkus-excludes/build.gradle.kts
@@ -16,29 +16,39 @@ val keycloakDist by configurations.creating
 
 dependencies {
     keycloakDist("org.keycloak:keycloak-quarkus-dist:$keycloakVersion@zip")
-    implementation("org.keycloak:keycloak-quarkus-server:$keycloakVersion") {
-        exclude("org.wildfly.security", "wildfly-elytron")
-
-        exclude("mysql", "mysql-connector-java")
-        exclude("io.quarkus", "quarkus-jdbc-mysql")
-        exclude("io.quarkus", "quarkus-jdbc-mysql-deployment")
-
-        exclude("com.microsoft.sqlserver", "mssql-jdbc")
-        exclude("io.quarkus", "quarkus-jdbc-mssql")
-        exclude("io.quarkus", "quarkus-jdbc-mssql-deployment")
-
-        exclude("com.oracle.database.jdbc", "ojdbc11")
-        exclude("io.quarkus", "quarkus-jdbc-oracle")
-        exclude("io.quarkus", "quarkus-jdbc-oracle-deployment")
-
-        exclude("org.mariadb.jdbc", "mariadb-java-client")
-        exclude("io.quarkus", "quarkus-jdbc-mariadb")
-        exclude("io.quarkus", "quarkus-jdbc-mariadb-deployment")
-
-        exclude("com.h2database", "h2")
-        exclude("io.quarkus", "quarkus-jdbc-h2")
-        exclude("io.quarkus", "quarkus-jdbc-h2-deployment")
+    implementation("org.keycloak:keycloak-quarkus-server:$keycloakVersion")
+    constraints {
+        implementation("org.keycloak:keycloak-core:$keycloakVersion")
+        implementation("org.jboss.resteasy:resteasy-client:4.7.5.Final")
+        implementation("com.thoughtworks.xstream:xstream:1.4.19")
+        implementation("org.postgresql:postgresql:42.3.5")
+        implementation("com.fasterxml.jackson.core:jackson-databind:2.13.2.2")
+        implementation("org.apache.commons:commons-compress:1.21")
     }
+}
+
+configurations.all {
+    exclude("org.wildfly.security", "wildfly-elytron")
+
+    exclude("mysql", "mysql-connector-java")
+    exclude("io.quarkus", "quarkus-jdbc-mysql")
+    exclude("io.quarkus", "quarkus-jdbc-mysql-deployment")
+
+    exclude("com.microsoft.sqlserver", "mssql-jdbc")
+    exclude("io.quarkus", "quarkus-jdbc-mssql")
+    exclude("io.quarkus", "quarkus-jdbc-mssql-deployment")
+
+    exclude("com.oracle.database.jdbc", "ojdbc11")
+    exclude("io.quarkus", "quarkus-jdbc-oracle")
+    exclude("io.quarkus", "quarkus-jdbc-oracle-deployment")
+
+    exclude("org.mariadb.jdbc", "mariadb-java-client")
+    exclude("io.quarkus", "quarkus-jdbc-mariadb")
+    exclude("io.quarkus", "quarkus-jdbc-mariadb-deployment")
+
+    exclude("com.h2database", "h2")
+    exclude("io.quarkus", "quarkus-jdbc-h2")
+    exclude("io.quarkus", "quarkus-jdbc-h2-deployment")
 }
 
 group = "com.example"
@@ -46,6 +56,11 @@ group = "com.example"
 java {
     sourceCompatibility = JavaVersion.VERSION_11
     targetCompatibility = JavaVersion.VERSION_11
+}
+
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
+    options.compilerArgs.add("-parameters")
 }
 
 ext {
