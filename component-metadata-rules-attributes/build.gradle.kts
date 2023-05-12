804bb4ff79e576f68d0c685001eead082225ef5b
Benedikt Ritter <benedikt@gradle.com>
2023-05-12 13:41:52 +0200

    Add example for component metadata rules and attributes

---
+++ component-metadata-rules-attributes/build.gradle.kts
@@ -0,0 +1,58 @@
+plugins {
+    `java-library`
+}
+
+repositories {
+    mavenCentral()
+    exclusiveContent {
+        forRepository {
+            ivy {
+                url = uri("https://github.com/aquasecurity/")
+                patternLayout {
+                    artifact("[module]/releases/download/v[revision]/[artifact]_[revision]_[classifier].[ext]")
+                }
+                metadataSources {
+                    artifact()
+                }
+            }
+        }
+        filter {
+            includeModule("aquasecurity", "trivy")
+        }
+    }
+}
+
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+}
+
+
+val trivy by configurations.creating
+
+dependencies {
+    components.all<CustomRule>()
+
+    trivy("aquasecurity:trivy:0.37.3:macOS-ARM64@tar.gz")
+    api("org.apache.commons:commons-lang3:3.11")
+}
+
+tasks.named<Test>("test") {
+    // Use JUnit Platform for unit tests.
+    useJUnitPlatform()
+}
+
+abstract class CustomRule : ComponentMetadataRule {
+
+    @get:Inject
+    abstract val objects: ObjectFactory
+
+    override fun execute(context: ComponentMetadataContext) {
+        println("Library: ${context.details.id.name}")
+        context.details.attributes.keySet().forEach {
+            println("$it:${context.details.attributes.getAttribute(it)}")
+        }
+    }
+
+}
